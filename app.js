f47d33474a28fea6f2fbfa9746f69f1e2cbd9de7
Andrebagus <andrefadli7@gmail.com>
2026-04-05 18:37:18 +0700

    second commit

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express');
-const client = require('prom-client'); // Library untuk metrik
+const client = require('prom-client'); 
 const app = express();
 const port = 3000;
 
-// 1. Setup pencatatan metrik otomatis (CPU, RAM, dll)
 const collectDefaultMetrics = client.collectDefaultMetrics;
 collectDefaultMetrics({ register: client.register });
 
-// 2. Custom metrik: Menghitung jumlah klik/request
 const counter = new client.Counter({
   name: 'jumlah_request_total',
   help: 'Menghitung total request yang masuk ke aplikasi anggota',
@@ -15,12 +13,10 @@ const counter = new client.Counter({
 });
 
 app.get('/', (req, res) => {
-  // Setiap ada yang akses, angka counter bertambah
-  counter.inc({ nama_anggota: 'Dico' }); // Ganti dengan nama kalian
+  counter.inc({ nama_anggota: 'Dico' });
   res.send('<h1>Aplikasi Anggota Kelompok 5 Berhasil Jalan!</h1>');
 });
 
-// 3. ENDPOINT WAJIB: Jalur untuk si Server mengambil data
 app.get('/metrics', async (req, res) => {
   res.set('Content-Type', client.register.contentType);
   res.end(await client.register.metrics());
